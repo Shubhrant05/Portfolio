af62e2ff73155d7baea529dbb3205eb49cd6026c
Shubhrant05 <90850886+Shubhrant05@users.noreply.github.com>
2021-12-25 13:20:18 +0530

    Delete Likes.js

--- src/components/Hero/Likes.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// const Likes = ({liked,liketext}) => {
-//     return (
-//         <button className="btn3" onClick={liked}>{liketext}</button>
-//     )
-// }
-
-// export default Likes
