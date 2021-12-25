4710e419347a43820998508e47ca82562aaebaf3
Shubhrant05 <90850886+Shubhrant05@users.noreply.github.com>
2021-12-25 13:11:26 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import About from "../components/About/About";
 import Contact from "../components/Contact/Contact";
-import Footer from "../components/Footer/Footer";
+// import Footer from "../components/Footer/Footer";
 import FixSocialIcon from "../components/SocialIcon/FixSocialIcon";
 function Home() {
   return (
@@ -12,7 +12,7 @@ function Home() {
       <Projects />
       <About />
       <Contact />
-      <Footer />
+//       <Footer />
       <FixSocialIcon />
     </>
   );
